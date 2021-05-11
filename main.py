ca98c2bd2a3f8e17097badce8f1359328c6fdbfb
dbachali <dbachali@stevens.edu>
2021-05-11 13:25:28 -0400

    building main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from sklearn import linear_model
 from sklearn.datasets import load_boston
+from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -14,6 +15,20 @@ boston['MEDV'] = boston_dataset.target
 def correlation():
     return boston.corr()
 
+def linear():
+    print("linear")
+
+def kmeans():
+    print("kmeans")
 
 if __name__ == '__main__':
-    correlation()
+    command = input('What do you want to do (enter "l" for linear regression, "k" for k-means clustering, "b" for both?')
+    if command == "l":
+        linear()
+    elif command == "k":
+        kmeans()
+    elif command == "b":
+        linear()
+        kmeans()
+    else:
+        print('Please enter either l, k, or b')
