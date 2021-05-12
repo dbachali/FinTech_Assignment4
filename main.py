b1f3526e928d4289fed427ef59806bfefcf55a36
dbachali <dbachali@stevens.edu>
2021-05-11 23:20:09 -0400

    typos

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def linear():
     This is NOT done here."""
     result = np.where(reg.coef_ == max(reg.coef_, key = abs))
     print('The factor with the most influence is: ', boston.columns[result[0][0]])
-    pprint('Looking at the result and what it stands for (ass well as correlation matrix of the data'
+    pprint('Looking at the result and what it stands for (as well as correlation matrix of the data'
            'it doesn\'t seem like NOX should be the most influential factor.  Then again, any major increase'
            'in nitric oxide concentration could make a location uninhabitable, so it is a fair result,'
            'we just normally don\'t see a wide range of NOX values in places where people can live.'
